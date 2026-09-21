075184d78bfcabfb05b600b830ed7a6936155279
Talha <iamtalhaali7766@gmail.com>
2026-09-22 02:52:29 +0500

    Fetch products and reviews using api's

---
+++ products.js
@@ -0,0 +1,41 @@
+function loadProducts() {
+  const productsContainer = document.querySelector(".my-products");
+  const reviews = document.querySelector(".reviews");
+
+  fetch("https://dummyjson.com/products")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.products.forEach((product) => {
+
+        productsContainer.innerHTML += `
+          <div class="bg-red-100 rounded-sm p-3">
+            <h2>Title: ${product.title}</h2>
+            <p>Description: ${product.description}</p>
+            <p>Price: ${product.price}</p>
+            <p>Rating: ${product.rating}</p>
+          </div>
+        `;
+
+        product.reviews.forEach((review) => {
+          reviews.innerHTML += `
+            <div class="bg-gray-800 p-3 rounded">
+              <p>${review.reviewerName}</p>
+              <p>${review.rating}</p>
+              <p>${review.comment}</p>
+              <p>${review.date}</p>
+              <p>${review.reviewerEmail}</p>
+            </div>
+          `;
+        });
+
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+loadProducts();
+
